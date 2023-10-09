9bb7f892fa9e21a34834a7e28a78cc3eae025884
RafiWick <rafiwick@gmail.com>
2023-10-09 11:56:30 -0600

    create booking endpoint made

---
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -0,0 +1,33 @@
+﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace CountryClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult CreateBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+            Response.StatusCode = 201;
+            return new JsonResult(booking);
+        }
+    }
+}
